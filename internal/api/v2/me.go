7963c9d55361c85d8bab9255f97f862194da0a6b
lxfontes <lucas.fontes@meetdandy.com>
2024-03-04 13:05:04 -0500

    logging

--- internal/api/v2/me.go
+++ internal/api/v2/me.go
@@ -45,6 +45,8 @@ func MeHandler(srv server.Server) http.Handler {
 			return
 		}
 
+    srv.Logger.Info("me request","user",userEmail)
+
 		switch r.Method {
 		// The HEAD method is used to determine if the user is currently
 		// authenticated.
